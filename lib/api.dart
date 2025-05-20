d0e61f20223e78445013db7c0ffd0d04c81d4a7f
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-05-20 13:27:16 +0000

    Added temporary random weather

--- lib/api.dart
+++ lib/api.dart
@@ -1,3 +1,48 @@
-int test() {
-  return 1000;
+import 'dart:math';
+
+abstract interface class Weather {
+  int getTemperature();
+  int getWindSpeed();
+
+  // Percentages
+  int getCloudCover();
+  int getHumidity();
+
+  // Metrics
+  int getAQI();
+  int getUV();
+}
+
+class RandomWeather implements Weather {
+  var rand = Random();
+
+  @override
+  int getTemperature() {
+    return rand.nextInt(20) + 10;
+  }
+
+  @override
+  int getWindSpeed() {
+    return rand.nextInt(20);
+  }
+
+  @override
+  int getCloudCover() {
+    return rand.nextInt(100);
+  }
+
+  @override
+  int getHumidity() {
+    return rand.nextInt(100);
+  }
+
+  @override
+  int getAQI() {
+    return rand.nextInt(100);
+  }
+
+  @override
+  int getUV() {
+    return rand.nextInt(100);
+  }
 }
