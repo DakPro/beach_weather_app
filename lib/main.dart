309a9b411d6425e408e1e3e6f77e390f11099bec
Amir <150358009+amirrezafazel@users.noreply.github.com>
2025-05-20 15:02:18 +0100

    Added suggestions.

--- lib/main.dart
+++ lib/main.dart
@@ -93,17 +93,40 @@ class HomePage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    color: Colors.black,
-                    height: 150,
-                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.black26,
+                      border: Border.all(color: Colors.black),
+                    ),
+                    height: 175,
+                    width: 175,
+                    child: Center(child: Text('Top page info')),
                   ),
-                  SizedBox(width: 10),
+                  SizedBox(width: 20),
                   Container(
-                    color: Colors.black,
-                    height: 150,
-                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.black26,
+                      border: Border.all(color: Colors.black),
+                    ),
+                    height: 175,
+                    width: 175,
+                    child: Center(child: Text('Image')),
                   ),
                 ],
+              ),
+              SizedBox(height: 20),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 18),
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.black26,
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Center(
+                  child: Text(
+                    'Suggestion',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
               ),
               SizedBox(height: 20),
               GridView.count(
