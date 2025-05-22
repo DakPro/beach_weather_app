963b774e36660f88fed7b0417887be2a259f72ab
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-05-22 20:48:03 +0000

    Add AQI

--- lib/weather/open_meteo.dart
+++ lib/weather/open_meteo.dart
@@ -1,7 +1,9 @@
 import 'package:open_meteo/open_meteo.dart';
 
 Future<OpenMeteo> init(double longitude, double latitude) async {
-  var response = await WeatherApi(
+  const timeout = Duration(seconds: 5);
+
+  var weather = await WeatherApi(
         temperatureUnit: TemperatureUnit.celsius,
         windspeedUnit: WindspeedUnit.kmh,
       )
@@ -22,59 +24,73 @@ Future<OpenMeteo> init(double longitude, double latitude) async {
         daily: {WeatherDaily.sunrise, WeatherDaily.sunset},
         forecastDays: 7,
       )
-      .timeout(Duration(seconds: 5));
+      .timeout(timeout);
 
-  return OpenMeteo(response);
+  var aqi = await AirQualityApi()
+      .request(
+        latitude: latitude,
+        longitude: longitude,
+        hourly: {AirQualityHourly.us_aqi},
+      )
+      .timeout(timeout);
+
+  return OpenMeteo(weather, aqi);
 }
 
 class OpenMeteo {
-  late ApiResponse<WeatherApi> response;
+  late ApiResponse<WeatherApi> weather;
+  late ApiResponse<AirQualityApi> aqi;
 
-  OpenMeteo(this.response);
+  OpenMeteo(this.weather, this.aqi);
 
   Map<DateTime, num> getTemperature() {
-    return response.hourlyData[WeatherHourly.temperature_2m]!.values;
+    return weather.hourlyData[WeatherHourly.temperature_2m]!.values;
   }
 
   Map<DateTime, num> getApparentTemperature() {
-    return response.hourlyData[WeatherHourly.apparent_temperature]!.values;
+    return weather.hourlyData[WeatherHourly.apparent_temperature]!.values;
   }
 
   Map<DateTime, num> getPressure() {
-    return response.hourlyData[WeatherHourly.pressure_msl]!.values;
+    return weather.hourlyData[WeatherHourly.pressure_msl]!.values;
   }
 
   Map<DateTime, num> getPrecipitation() {
-    return response.hourlyData[WeatherHourly.precipitation]!.values;
+    return weather.hourlyData[WeatherHourly.precipitation]!.values;
   }
 
   Map<DateTime, num> getPrecipitationProbability() {
-    return response.hourlyData[WeatherHourly.precipitation_probability]!.values;
+    return weather.hourlyData[WeatherHourly.precipitation_probability]!.values;
   }
 
   Map<DateTime, num> getCloudCover() {
-    return response.hourlyData[WeatherHourly.cloud_cover]!.values;
+    return weather.hourlyData[WeatherHourly.cloud_cover]!.values;
   }
 
   Map<DateTime, num> getVisibility() {
-    return response.hourlyData[WeatherHourly.visibility]!.values;
+    return weather.hourlyData[WeatherHourly.visibility]!.values;
   }
 
   Map<DateTime, num> getWindSpeed() {
-    return response.hourlyData[WeatherHourly.wind_speed_10m]!.values;
+    return weather.hourlyData[WeatherHourly.wind_speed_10m]!.values;
   }
 
   // First day is today
   List<DateTime> getSunrise() {
-    return response.dailyData[WeatherDaily.sunrise]!.values.values
+    return weather.dailyData[WeatherDaily.sunrise]!.values.values
         .map((x) => DateTime.fromMillisecondsSinceEpoch((x * 1000).toInt()))
         .toList();
   }
 
   List<DateTime> getSunset() {
-    return response.dailyData[WeatherDaily.sunset]!.values.values
+    return weather.dailyData[WeatherDaily.sunset]!.values.values
         .map((x) => DateTime.fromMillisecondsSinceEpoch((x * 1000).toInt()))
         .toList();
+  }
+
+  Map<DateTime, num> getAQI() {
+    // Many AQI measures available. Use US one for now
+    return aqi.hourlyData[AirQualityHourly.us_aqi]!.values;
   }
 }
 
@@ -89,4 +105,5 @@ void main() async {
   var a = await init(0.0, 52.0);
   var now = DateTime.now();
   print(a.getSunset());
+  print(a.getAQI());
 }
