c835ee5ed6e3fa1b5287e3f2017d355004c4bbb6
arissaelena <arissa.rotunjanu@gmail.com>
2025-05-24 17:48:21 +0100

    AQI page done

--- lib/element_pages.dart
+++ lib/element_pages.dart
@@ -3,7 +3,7 @@ import 'package:fl_chart/fl_chart.dart';
 
 class WindSpeedPage extends StatelessWidget {
   const WindSpeedPage({super.key, required this.speed});
-  final String speed;
+  final Map speed;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,7 +84,7 @@ class WindSpeedPage extends StatelessWidget {
 
 class PressurePage extends StatelessWidget {
   const PressurePage({super.key, required this.pressure});
-  final String pressure;
+  final Map pressure;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -246,7 +246,7 @@ class WavesPage extends StatelessWidget {
 
 class AQIPage extends StatelessWidget {
   const AQIPage({super.key, required this.index});
-  final String index;
+  final Map index;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -297,7 +297,8 @@ class AQIPage extends StatelessWidget {
                 ),
                 SizedBox(height: 10),
                 Container(
-                    height: 460,
+                    padding: const EdgeInsets.all(12.0),
+                    height: 500,
                     alignment: Alignment.center,
                     decoration: BoxDecoration(
                       color: Color(0xFF264864).withValues(alpha: 0.8),
@@ -310,16 +311,45 @@ class AQIPage extends StatelessWidget {
                         ),
                       ],
                     ),
-                    child: Text(
-                      'Weather information...',
-                      style: TextStyle(),
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Current AQI: ${index.values.first.round()}.\n\n',
+                        style: TextStyle(
+                        fontSize: 14,
+                        height: 1.5,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF121418),
+                      ),
+                        children: const <TextSpan>[
+                          TextSpan(text: 'What is AQI for?\n', style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Color(0xFF121418))),
+                          TextSpan(text: 'The air quality index is a tool for communicating about the outdoor air quality and its effects on health. The higher the AQI value, the greater the level of air pollution and increase in health risks.\n\n', style: TextStyle(fontSize: 12, color: Color(0xFF121418))),
+                          TextSpan(text: 'Good (0 to 50): Air quality is satisfactory, and air pollution poses little or no risk.\n'
+                          'Moderate (51 to 100): Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.\n'
+                          'Unhealthy for Sensitive Groups (101 to 150): Members of sensitive groups may experience health effects. The general public is less likely to be affected.\n'
+                          'Unhealthy (151 to 200): Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.\n'
+                          'Very Unhealthy (201 to 300): The risk of health effects is increased for everyone.\n'
+                          'Hazardous (301 and higher): Health warnings for emergency conditions, everyone is more likely to be affected.', style: TextStyle(fontSize: 12, color: Color(0xFF121418)))
+                        ],
+                      ),
                     )
-                )
+                ),
               ]
               ),
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(12),
+        height: 50.0,
+        color: Color(0xFF263644),
+        child: Text('Sourced from: AQI Basics | AirNow.gov. (n.d.). https://www.airnow.gov/aqi/aqi-basics/',
+        style: TextStyle(
+            color: Color(0xFF121418),
+        fontSize: 10,
+        fontWeight: FontWeight.bold,
+        ),
+      ),
       ),
     );
   }
@@ -406,9 +436,9 @@ class TidePage extends StatelessWidget {
   }
 }
 
-class HumidityPage extends StatelessWidget {
-  const HumidityPage({super.key, required this.humidity});
-  final String humidity;
+class PrecipitationPage extends StatelessWidget {
+  const PrecipitationPage({super.key, required this.prec, required this.precp});
+  final Map prec, precp;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -489,7 +519,7 @@ class HumidityPage extends StatelessWidget {
 
 class CloudCoveragePage extends StatelessWidget {
   const CloudCoveragePage({super.key, required this.coverage});
-  final String coverage;
+  final Map coverage;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -570,7 +600,7 @@ class CloudCoveragePage extends StatelessWidget {
 
 class SunsetPage extends StatelessWidget {
   const SunsetPage({super.key, required this.time});
-  final String time;
+  final List time;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -651,7 +681,7 @@ class SunsetPage extends StatelessWidget {
 
 class SunrisePage extends StatelessWidget {
   const SunrisePage({super.key, required this.time});
-  final String time;
+  final List time;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -731,8 +761,8 @@ class SunrisePage extends StatelessWidget {
 }
 
 class TemperaturePage extends StatelessWidget {
-  const TemperaturePage({super.key, required this.temp});
-  final String temp;
+  const TemperaturePage({super.key, required this.temp, required this.atemp});
+  final Map temp, atemp;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -894,7 +924,7 @@ class UVPage extends StatelessWidget {
 
 class VisibilityPage extends StatelessWidget {
   const VisibilityPage({super.key, required this.vis});
-  final String vis;
+  final Map vis;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
